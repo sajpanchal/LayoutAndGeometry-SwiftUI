abc609f1da8eced244f1ddac7b91cf7a77605cf4
Saj Panchal <sajpanchal@gmail.com>
2021-09-06 12:53:47 -0400

    ScrollView effects using geometryReader.

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
     var body: some View {
-       OuterView()
-        .background(Color.red)
-        .coordinateSpace(name: "Custom")
-        
+        GeometryReader { fullView in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0..<50) {index in
+                        GeometryReader { geo in
+                            Rectangle()
+                                .fill(self.colors[index % 7])
+                                .frame(height: 150)
+                                .rotation3DEffect(
+                                    .degrees(-Double(geo.frame(in: .global).midX - fullView.size.width / 2) / 10),
+                                    axis: (x: 0.0, y: 1.0, z: 0.0)
+                                )
+                        }
+                        .frame(width: 150)
+                    }
+                }
+                .padding(.horizontal, (fullView.size.width - 150) / 2)
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
